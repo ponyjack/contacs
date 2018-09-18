f5e7856336c7625e46254cb0d329d496b5464ee7
zhou cheng <zhoucheng@kingsoft.com>
2018-09-18 20:27:31 +0800

    add log

--- contacts/contacts/api.py
+++ contacts/contacts/api.py
@@ -1,45 +1,41 @@
 from rest_framework import routers, serializers, viewsets
-from .models import Person, Company, PhoneNumber
+from .models import Person, PhoneNumber
 import logging
 
 logger = logging.getLogger(__name__)
 # Serializers define the API representation.
 
 
-class PhoneNumberObjectRelatedField(serializers.RelatedField):
-    """
-    A custom field to use for the `tagged_object` generic relationship.
-    """
-
-    def to_representation(self, value):
-        """
-        Serialize tagged objects to a simple textual representation.
-        """
-        if isinstance(value, Company):
-            serializer = CompanySerializer(value)
-        elif isinstance(value, Person):
-            serializer = PersonSerializer(value)
-        return serializer.data
+class PhoneNumberSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PhoneNumber
+        exclude = ()
 
 
 class PersonSerializer(serializers.ModelSerializer):
     #
+    phones = PhoneNumberSerializer(many=True, required=False)
 
     class Meta:
         model = Person
         exclude = ()
 
     def create(self, validated_data):
-        logger.info(validated_data)
-        persons = validated_data.pop("phone_number", None)
-        logger.info(persons)
-        company = Company.objects.create(**validated_data)
+        phones = validated_data.pop("phones", [])
 
-        if persons:
-            for v in persons:
-                p, created = Person.objects.get_or_create(company=company, **v)
-                logger.info(created)
-        return company
+        person = Person.objects.create(**validated_data)
+
+        for p in phones:
+            PhoneNumber.objects.create(person=person, **p)
+        return person
+
+    def update(self, instance, validated_data):
+        phones = validated_data.pop("phones", [])
+        logger.info(PhoneNumber._meta.pk.name)
+        person = Person.objects.create_or_update(**validated_data)
+        logger.info(phones)
+        for p in phones:
+            logger.info(p.get("id", None))
 
 
 class PersonViewSet(viewsets.ModelViewSet):
@@ -47,33 +43,7 @@ class PersonViewSet(viewsets.ModelViewSet):
     serializer_class = PersonSerializer
 
 
-class CompanySerializer(serializers.ModelSerializer):
-    persons = PersonSerializer(many=True, read_only=True)
-
-    class Meta:
-        model = Company
-        exclude = ()
-
-    def create(self, validated_data):
-        logger.info(validated_data)
-        persons = validated_data.pop("persons", None)
-        logger.info(persons)
-        company = Company.objects.create(**validated_data)
-
-        if persons:
-            for v in persons:
-                p, created = Person.objects.get_or_create(company=company, **v)
-                logger.info(created)
-        return company
-
-
-class CompanyViewSet(viewsets.ModelViewSet):
-    queryset = Company.objects.all()
-    serializer_class = CompanySerializer
-
-
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
 router.register(r"person", PersonViewSet)
-router.register(r"company", CompanyViewSet)
 
--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import requests
 import pprint
 
+phones = [{"phone_number": "234234", "name": "123123", "slug": "12312"}]
 person = {
     "first_name": "123",
     "last_name": "123",
@@ -9,12 +10,10 @@ person = {
     "nickname": "123",
     "title": "123",
     "about": "123",
-    "id": 1,
-    "slug": "sdfdsf",
-    "phone_number": [{"phone_number": "12312313123"}],
+    "phones": phones,
 }
-data = {"name": "123", "nickname": "123", "about": "123", "persons": [person]}
-print(data)
+data = {"name": "123", "nickname": "123", "about": "123", "persons": [phones]}
+print(person)
 import json
 
 headers = {"Content-Type": "application/json"}
@@ -26,8 +25,8 @@ headers = {"Content-Type": "application/json"}
 
 # print(r.json())
 
-r = requests.post(
-    "http://127.0.0.1:8000/user/person/", data=json.dumps(person), headers=headers
+r = requests.put(
+    "http://127.0.0.1:8000/user/person/1/", data=json.dumps(person), headers=headers
 )
 print(r)
 
